21ba29fec79dc193b9c8fbea60449a41369e9089
Brooke Hewitt <dnj6xk@virginia.edu>
2023-12-01 16:10:42 -0500

    Added list of movies

--- Project/index.php
+++ Project/index.php
@@ -7,3 +7,23 @@ require("utils.php");
 
 $list = getAllMovieInfo();
 ?>
+
+<h3>List of Movies</h3>
+<div class="row justify-content center">
+    <table class="w3-table w3-bordered w3-card-4 center" style="width:70%">
+    <thead>
+        <tr style="background-color:#B0B0B0">
+            <th width="30%">Title
+            <th width="30%">Runtime
+            <th width="30%">Age Rating
+        </tr>
+    </thead>
+    <?php foreach ($list as $movie): ?>
+        <tr>
+            <td><?php echo $movie['title']; ?></td>
+            <td><?php echo $movie['runtime']; ?></td>
+            <td><?php echo $movie['age_rating']; ?></td>
+    </tr>
+    <?php endforeach; ?>
+    </table>
+</div>
