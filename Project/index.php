30187fb8e3e5121c8ce9ccc2a69062ebd99f0df7
Brooke Hewitt <dnj6xk@virginia.edu>
2023-12-01 15:55:25 -0500

    Removed unecessary file

---
+++ Project/index.php
@@ -0,0 +1,13 @@
+<?php
+
+session_start();
+
+#require("connect-db.php")
+#require("utils.php")
+
+#$list = getAllMovieInfo();
+?>
+
+<form name="mainForm" action="main.php" action="post">
+        Hello
+</form>
