02db7330eb240980ff5a978ea34f6598a257c8ab
Brooke Hewitt <dnj6xk@virginia.edu>
2023-12-01 14:44:17 -0500

    Added db login info

--- Project/connect-db.php
+++ Project/connect-db.php
@@ -43,8 +43,8 @@
 
 /** F23, PHP (on GCP, local XAMPP, or CS server) connect to MySQL (on CS server) **/
 
-$username = ''; //FIX THESE
-$password = '';
+$username = 'dnj6xk'; //FIX THESE
+$password = 'Superc00l1';
 $host = 'mysql01.cs.virginia.edu';
 $dbname = '';
 $dsn = "mysql:host=$host;dbname=$dbname";
