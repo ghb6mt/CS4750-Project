8590f7bfd0492eaf3f2ca2afbbf641eda892b079
Brooke Hewitt <dnj6xk@virginia.edu>
2023-12-02 19:36:06 -0500

    Search functionality for movie title working

--- Project/index.php
+++ Project/index.php
@@ -12,12 +12,12 @@ include('navbar.html')
 <?php
     if(isset($_POST['search'])) {
         $searchq = $_POST['search'];
-        $query = mysql_query("SELECT * FROM movies NATURAL JOIN genres NATURAL JOIN lead_actors WHERE title LIKE '%$searchq%'");
+        $query = "SELECT * FROM movies NATURAL JOIN genres NATURAL JOIN lead_actors WHERE title LIKE '%$searchq%'";
         $statement = $db->prepare($query);
         $statement->execute();
-        $showings = $statement->fetchAll();
+        $filtered = $statement->fetchAll();
         $statement->closeCursor();
-        echo $showings;
+        echo $filtered;
     }
 ?>
 
@@ -25,6 +25,31 @@ include('navbar.html')
     <input type="text" name="search" placeholder="Search by movie title">
     <input type="submit" value="Search">
 </form>
+
+<div class="row justify-content center">
+    <table class="w3-table w3-bordered w3-card-4 center" style="width:70%">
+    <thead>
+        <tr style="background-color:#B0B0B0">
+            <th width="30%">Movie Title
+            <th width="30%">Lead Actor
+            <th width="30%">Age Rating
+            <th width="30%">Genre
+            <th width="30%">Runtime
+        </tr>
+    </thead>
+    <?php foreach ($filtered as $movie): ?>
+        <tr>
+            <td><?php echo $movie['title']; ?></td>
+            <?php $leadActor = getMovieLeadActor($movie['movie_id']); ?>
+            <td><?php echo $leadActor[0][0] ?></td>
+            <td><?php echo $movie['age_rating']; ?></td>
+            <?php $genres = getMovieGenre($movie['movie_id']); ?>
+            <td><?php echo $genres[0][0] ?></td>
+            <td><?php echo $movie['runtime']; ?></td>
+    </tr>
+    <?php endforeach; ?>
+    </table>
+</div>
 
 <h3>List of Movies</h3>
 <div class="row justify-content center">
