a887df5a216ca30d138f72c446e8b97bf4d66318
Brooke Hewitt <dnj6xk@virginia.edu>
2023-12-03 01:25:31 -0500

    Expanded search bar

--- Project/index.php
+++ Project/index.php
@@ -10,10 +10,59 @@ include('navbar.html')
 ?>
 
 <?php
-    if(isset($_POST['search'])) {
-        $searchq = $_POST['search'];
-        $filterby = $_POST['filter'];
-        $query = "SELECT * FROM movies NATURAL JOIN genres NATURAL JOIN lead_actors WHERE $filterby LIKE '%$searchq%'";
+    if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+        $query = "";
+        if(isset($_POST['title'])) {
+            $title = $_POST['title'];
+            if(empty($query)) {
+                $query = "SELECT * FROM movies NATURAL JOIN genres NATURAL JOIN lead_actors WHERE title LIKE '%$title%'";
+            }
+            else {
+                $query = $query . " AND title LIKE '%$title%'";
+            }
+        }
+        if(isset($_POST['actor'])) {
+            $actor = $_POST['actor'];
+            if(empty($query)) {
+                $query = "SELECT * FROM movies NATURAL JOIN genres NATURAL JOIN lead_actors WHERE lead_actor LIKE '%$actor%'";
+            }
+            else {
+                $query = $query . " AND lead_actor LIKE '%$actor%'";
+            }
+        }
+        if(isset($_POST['rating'])) {
+            $rating = $_POST['rating'];
+            if ($rating != "none") {
+                if(empty($query)) {
+                    $query = "SELECT * FROM movies NATURAL JOIN genres NATURAL JOIN lead_actors WHERE age_rating = '$rating'";
+                }
+                else {
+                    $query = $query . " AND age_rating = '$rating'";
+                }
+            }
+        }
+        if(isset($_POST['genre'])) {
+            $genre = $_POST['genre'];
+            if ($genre != "none") {
+                if(empty($query)) {
+                    $query = "SELECT * FROM movies NATURAL JOIN genres NATURAL JOIN lead_actors WHERE genre = '$genre'";
+                }
+                else {
+                    $query = $query . " AND genre = '$genre'";
+                }
+            }
+        }
+        if(isset($_POST['runtime'])) {
+            if (!ctype_alpha($_POST['runtime'])) {
+                $runtime = intval($_POST['runtime']);
+                if(empty($query)) {
+                    $query = "SELECT * FROM movies NATURAL JOIN genres NATURAL JOIN lead_actors WHERE runtime < $runtime";
+                }
+                else {
+                    $query = $query . " AND runtime < $runtime";
+                }
+            }
+        }
         $statement = $db->prepare($query);
         $statement->execute();
         $filtered = $statement->fetchAll();
@@ -22,14 +71,25 @@ include('navbar.html')
 ?>
 
 <form method="POST" action="index.php">
-    <input type="text" name="search" placeholder="Search by movie title">
-    <select name="filter" id="filter">
-        <option value="title">Movie Title</option>
-        <option value="lead_actor">Lead Actor</option>
-        <option value="age">Age Rating</option>
-        <option value="genre">Genre</option>
-        <option value="runtime">Runtime</option>
+    <input type="text" name="title" placeholder="Search by movie title">
+    <input type="text" name="actor" placeholder="Search by lead actor">
+    <select name="rating" id="rating">
+        <option value="none">Age Rating</option>
+        <option value="g">G</option>
+        <option value="pg">PG</option>
+        <option value="pg-13">PG-13</option>
+        <option value="r">R</option>
     </select>
+    <select name="genre" id="genre">
+        <option value="none">Genre</option>
+        <option value="action">Action</option>
+        <option value="adventure">Adventure</option>
+        <option value="comedy">Comedy</option>
+        <option value="drama">Drama</option>
+        <option value="musical">Musical</option>
+        <option value="sports">Sports</option>
+    </select>
+    <input type="text" name="runtime" placeholder="Runtime less than...">
     <input type="submit" value="Search">
 </form>
 
